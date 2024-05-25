b2491bb69088f554c7fc1d551b05f41e486622cb
генгста <https://github.com/Atabek200/test12.git@>
2024-05-25 22:20:16 +0300

    reee

---
+++ academy/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from .views import master_signup, client_signup, user_login
+
+urlpatterns = [
+    path('main/', views.main, name='home'),
+    path('serif/', views.serif, name='serif'),
+    path('', views.register, name='register'),
+    path('signup/master/', master_signup, name='master_signup'),
+    path('signup/client/', client_signup, name='client_signup'),
+    path('login/', user_login, name='login'),
+]
